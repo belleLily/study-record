1d5339858bcd11d32b3368113e0f36dd7e068980
wanlili <251808939@qq.com>
2022-01-19 16:13:12 +0800

    add: 2022-01-19

--- leetcode/53.最大子数组和.js
+++ leetcode/53.最大子数组和.js
@@ -1,13 +1,16 @@
+// 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+// 子数组 是数组中的一个连续部分。
+
 // 我不理解
-var maxSubArray = function (nums) {
-  let pre = 0,
-    maxAns = nums[0];
-  nums.forEach((x) => {
-    pre = Math.max(pre + x, x);
-    maxAns = Math.max(maxAns, pre);
-  });
-  return maxAns;
-};
+// var maxSubArray = function (nums) {
+//   let pre = 0,
+//     maxAns = nums[0];
+//   nums.forEach((x) => {
+//     pre = Math.max(pre + x, x);
+//     maxAns = Math.max(maxAns, pre);
+//   });
+//   return maxAns;
+// };
 
 // var maxSubArray = function (nums) {
 //   var max = 0;
@@ -18,4 +21,18 @@ var maxSubArray = function (nums) {
 //   });
 //   return max;
 // };
+
+var maxSubArray = function (nums) {
+  var max = nums[0];
+  var sum = 0;
+  nums.forEach((x) => {
+    if (sum > 0) {
+      sum += x;
+    } else {
+      sum = x;
+    }
+    max = Math.max(sum, max);
+  });
+  return max;
+};
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4, 3]));
---
+++ leetcode/58.最后一个单词的长度.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  var res = s.split(/\s+/).filter((item) => !!item);
+  return res[res.length - 1].length;
+};
+
+var lengthOfLastWord = function (s) {
+    var idx = s.length - 1;
+    while (s[idx] === " ") {
+      idx--;
+    }
+    var wordLen = 0;
+    while (idx >= 0 && s[idx] !== ' ') {
+        wordLen++
+        idx--
+    }
+    return wordLen
+};
+console.log(lengthOfLastWord("   fly me   to   the moon  "));
+console.log(lengthOfLastWord("luffy is still joyboy"));
